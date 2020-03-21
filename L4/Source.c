90ac41c9bc4b4954e93e7575d6ae43e7ee1a5aab
Stanca-Catalin <62239377+Stanca-Catalin@users.noreply.github.com>
2020-03-21 13:46:18 +0200

    Create Source.c

---
+++ L4/Source.c
@@ -0,0 +1,31 @@
+// P2
+#include<stdio.h>
+#include<stdlib.h>
+
+int suma(int n, int V[20])
+{
+	int i t = 0;
+	for (i = 0; i <=n; i++)
+	{
+		k = k + V[i]
+	}
+	return k;
+}
+int main()
+{
+	int n, i, *V, S;
+	printf("n=");
+	scanf("%d",&n);
+	V = (int *)malloc(n * sizeof(int));
+	for (i = 0; i<n; i++)
+	{
+		printf("V[%d]=", i + 1);
+		scanf("%d", (V + i));
+	}
+	S = 0;
+	S = suma(n, v);
+	printf('%d', S);
+	system("pause");
+	return 0;
+	}
+}
